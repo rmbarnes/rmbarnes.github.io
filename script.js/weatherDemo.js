d2160256e408a4a689a8419f5cce63fd73ff807a
rmbarnes <rachelbarnes@Rachels-MacBook-Pro.local>
2017-03-16 18:22:39 -0600

    JSON demonstration

---
+++ script.js/weatherDemo.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function ($) {
+    $.ajax({
+        url: "http://api.wunderground.com/api/7f040ad384cf4dfb/forecast/geolookup/conditions/q/SC/Greenville.json",
+        dataType: "jsonp",
+        success: function (data) {
+            var location = data['Greenville']['city'];
+            var temp_f = data['Greenville']['high'];
+            console.log("Current temperature in " + location + " is: " + temp_f);
+
+            var locName = $('#locName');
+            var highTemp = $('#highTemp');
+            var tempMessage = $('#tempMessage');
+
+            locName.text(location);
+            highTemp.text(temp_f);
+            tempMessage.text("Current temperature in " + location + " is: " + temp_f);
+        }
+    });
+});
